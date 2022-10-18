fef619cbffe87f7035c04cbe8298189ed977953b
James <james121op@gmail.com>
2022-10-19 00:04:45 +0100

    initial commit

---
+++ LogToConsole/LogToConsole.cs
@@ -0,0 +1,24 @@
+﻿using BrokeProtocol.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace _LogToConsole
+{
+    public class LogToConsole : Plugin
+    {
+        public LogToConsole()
+        {
+            Info = new PluginInfo("LogToConsole", "LogToConsole");
+            Application.logMessageReceived += Application_logMessageReceived;
+        }
+
+        private void Application_logMessageReceived(string logContents, string stackTrace, LogType type)
+        {
+            System.IO.File.AppendAllText("InternalConsole", String.IsNullOrWhiteSpace(stackTrace) ? $"{logContents}\n" : $"{logContents}\n{stackTrace}\n");
+        }
+    }
+}
